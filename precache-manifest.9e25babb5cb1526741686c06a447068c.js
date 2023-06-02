f84ab66622dfd6012ee61f7779bab1f751c31476
jomira0220 <119592838+jomira0220@users.noreply.github.com>
2023-06-02 10:06:46 +0900

    Updates

---
+++ precache-manifest.9e25babb5cb1526741686c06a447068c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bddecbac0d5b316d1a8ed5e4f58a77",
+    "url": "/react_movie/index.html"
+  },
+  {
+    "revision": "bc8a61ed19c05483509e",
+    "url": "/react_movie/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "d82eeba472549bfb73b9",
+    "url": "/react_movie/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "bc8a61ed19c05483509e",
+    "url": "/react_movie/static/js/2.9b330dda.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/react_movie/static/js/2.9b330dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82eeba472549bfb73b9",
+    "url": "/react_movie/static/js/main.93712277.chunk.js"
+  },
+  {
+    "revision": "6604b02de4bca117d62d",
+    "url": "/react_movie/static/js/runtime-main.f881bba8.js"
+  }
+]);
